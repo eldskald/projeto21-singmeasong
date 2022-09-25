651d3fafb407a960f89cf3073b8f7144a58eb483
rafael <rafaelbordoni00@gmail.com>
2022-09-25 13:02:54 -0300

    Update insert unit test to achieve 100% coverage

--- back-end/tests/unit/insertService.test.ts
+++ back-end/tests/unit/insertService.test.ts
@@ -1,21 +1,38 @@
 import { recommendationRepository as repository } from '../../src/repositories/recommendationRepository';
 import { recommendationService as service, CreateRecommendationData } from '../../src/services/recommendationsService';
-import * as errorUtils from '../../src/utils/errorUtils';
 import recommendationFactory from '../../prisma/factories/recommendationFactory';
 
-it('Testing insert()...', async () => {
-  jest.spyOn(repository, 'findByName').mockImplementation(() => null);
-  jest.spyOn(errorUtils, 'conflictError').mockImplementation(() => null);
+describe('Testing insert()...', () => {
   jest.spyOn(repository, 'create').mockImplementation(
     async (data: CreateRecommendationData) => { return; }
   );
-  let result: boolean = true;
-  const rec: CreateRecommendationData = recommendationFactory();
-  try {
-    await service.insert(rec)
-  } catch (_err) {
-    result = false;
-  }
-  expect(result).toBe(true);
-  expect(repository.create).toBeCalled();
+
+  it('Testing with a succesful insertion', async () => {
+    const rec: CreateRecommendationData = recommendationFactory();
+    jest.spyOn(repository, 'findByName').mockImplementation(() => null);
+    let result: boolean = true;
+    try {
+      await service.insert(rec)
+    } catch (_err) {
+      result = false;
+    }
+    expect(result).toBe(true);
+    expect(repository.create).toBeCalled();
+  });
+
+  it('Testing an insertion of a recommendation with a same name', async () => {
+    const rec: CreateRecommendationData = recommendationFactory();
+    jest.spyOn(repository, 'findByName').mockImplementation(
+      (_name: string): any => ({ ...rec, id: 1, score: 0 })
+    );
+    let result: boolean = true;
+    try {
+      await service.insert(rec);
+      result = false;
+    } catch (err) {
+      if (err.type != 'conflict') result = false;
+    }
+    expect(result).toBe(true);
+    expect(repository.create).toBeCalled();
+  });
 });
