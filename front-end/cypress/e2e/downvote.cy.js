1e1746c01088001a053a4bf373b7ee64451d29e8
rafael <rafaelbordoni00@gmail.com>
2022-09-26 02:49:30 -0300

    Add downvote e2e test

---
+++ front-end/cypress/e2e/downvote.cy.js
@@ -0,0 +1,11 @@
+beforeEach(() => {
+  cy.request('POST', 'http://localhost:4000/ready-db');
+});
+
+it('Should upvote a song successfully', () => {
+  cy.visit('http://localhost:3000');
+  cy.intercept('POST', 'http://localhost:4000/recommendations/1/downvote').as('downvote');
+  cy.get('[data-cy="rec1-downvote"').click();
+  cy.wait('@downvote');
+  cy.get('[data-cy="rec1-score"').should('have.text', '17');
+});
